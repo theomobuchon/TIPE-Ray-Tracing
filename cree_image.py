c541f44c7ea8e3f0a789f76127cfad1752b09a27
PierreJLM <pierre.jan.lm@orange.fr>
2025-09-30 11:39:35 +0200

    branche python

---
+++ cree_image.py
@@ -0,0 +1,22 @@
+
+
+
+im = open("P:/Documents/TIPE/python_ver/restultat.ppm", "w")
+
+image_height = 256
+image_width = 256
+im.write(f"P3\n {image_width} {image_height}\n255\n")
+
+for x in range(image_height):
+    for y in range(image_width):
+        r = x / (image_width-1)
+        g = y / (image_height-1)
+        b = 0.0
+
+        ir = int(255.999 * r)
+        ig = int(255.999 * g)
+        ib = int(255.999 * b)
+
+        im.write(f"{ir} {ig} {ib}\n")
+
+im.close()
