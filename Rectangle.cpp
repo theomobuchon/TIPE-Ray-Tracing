563cfe50bd0f2861feaae9ddc2cf6a0c75104dd6
MorphinT <morphin.tornado@gmail.com>
2026-04-20 15:33:43 +0200

    Resolution bug bbox rectangles

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -3,10 +3,33 @@
 //
 
 #include "Rectangle.hpp"
+#include <algorithm>
 
 Rectangle::Rectangle(const Point3 &origin, const Vec3 &u, const Vec3 &v, const std::shared_ptr<Material> &material): m_origin(origin), m_u(u), m_v(v), m_material(material) {
     m_normal = normalised(p_vect(u, v));
-    m_bbox = AABB(origin + m_normal * 0.001, origin + u + v - m_normal * 0.001);
+    Point3 p0 = m_origin;
+    Point3 p1 = m_origin + m_u;
+    Point3 p2 = m_origin + m_v;
+    Point3 p3 = m_origin + m_u + m_v;
+
+    Vec3 min_v(
+        std::min({p0.x(), p1.x(), p2.x(), p3.x()}),
+        std::min({p0.y(), p1.y(), p2.y(), p3.y()}),
+        std::min({p0.z(), p1.z(), p2.z(), p3.z()})
+    );
+
+    Vec3 max_v(
+        std::max({p0.x(), p1.x(), p2.x(), p3.x()}),
+        std::max({p0.y(), p1.y(), p2.y(), p3.y()}),
+        std::max({p0.z(), p1.z(), p2.z(), p3.z()})
+    );
+
+    constexpr float eps = 1e-4f;
+
+    min_v -= Vec3(eps, eps, eps);
+    max_v += Vec3(eps, eps, eps);
+
+    m_bbox = AABB(min_v, max_v);
 }
 
 bool Rectangle::hit(const Ray &ray, const Interval int_valid, Hit_record &rec) const {
